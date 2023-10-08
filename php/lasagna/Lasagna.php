2bf5085d51449e07f4b614fc7180cab3a373c422
Ctrl-Vinayak <vinayakshuklakalapnat@live.nl>
2023-10-08 21:40:27 +0200

    Update Lasagna.php 2e iteration

--- php/lasagna/Lasagna.php
+++ php/lasagna/Lasagna.php
@@ -4,28 +4,38 @@ declare(strict_types=1);
 
 class Lasagna
 {
+
+    public function __construct(
+        protected int $cookTime = 40,
+    ) {}
+
     // Please define the 'expectedCookTime()' function
-    function expectedCookTime() {
-        return 40;
+    function expectedCookTime(): int 
+    {
+        return $this->cookTime;
     }
 
     // Please define the 'remainingCookTime($elapsed_minutes)' function
-    function remainingCookTime($elapsed_minutes) {
-        return Lasagna::expectedCookTime() - $elapsed_minutes;
+    function remainingCookTime(int $elapsed_minutes): int
+    {
+        return $this -> expectedCookTime() - $elapsed_minutes;
     }
 
     // Please define the 'totalPreparationTime($layers_to_prep)' function
-    function totalPreparationTime($layers_to_prep) {
+    function totalPreparationTime(int $layers_to_prep): int
+    {
         return 2 * $layers_to_prep;
     }
 
     // Please define the 'totalElapsedTime($layers_to_prep, $elapsed_minutes)' function
-    function totalElapsedTime($layers_to_prep, $elapsed_minutes) {
-        return Lasagna::totalPreparationTime($layers_to_prep) + $elapsed_minutes;
+    function totalElapsedTime(int $layers_to_prep, int $elapsed_minutes):int
+    {
+        return $this -> totalPreparationTime($layers_to_prep) + $elapsed_minutes;
     }
 
     // Please define the 'alarm()' function
-    function alarm() {
+    function alarm():string
+    {
         return "Ding!";
     }
 }
