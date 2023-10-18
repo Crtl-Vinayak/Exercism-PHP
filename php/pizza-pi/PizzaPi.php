8561ae24b2adb740296c39d91aa3271dbb15d651
Ctrl-Vinayak <vinayakshuklakalapnat@live.nl>
2023-10-18 16:41:16 +0200

    pizzapi exercise, need a little of improvement in this code.

--- php/pizza-pi/PizzaPi.php
+++ php/pizza-pi/PizzaPi.php
@@ -2,6 +2,10 @@
 
 class PizzaPi
 {
+
+    public $SAUCE_PER_PIZZA = 125;
+    public $NUMBER_OF_SLICES = 8;
+
     public function calculateDoughRequirement(int $pizzas, int $persons): int
     {
         $grams = $pizzas * (($persons * 20) + 200);
@@ -9,19 +13,31 @@ class PizzaPi
         throw new \BadFunctionCallException("Implement the function");
     }
 
-    public function calculateSauceRequirement()
+    public function calculateSauceRequirement(int $pizzas, int $can_volume)
     {
+        return $pizzas * $this->SAUCE_PER_PIZZA / $can_volume;
         throw new \BadFunctionCallException("Implement the function");
     }
 
-    public function calculateCheeseCubeCoverage()
+    public function calculateCheeseCubeCoverage(int $length, float $thickness, int $diameter)
     {
+        return floor($length**3 / ($thickness * pi() * $diameter));
         throw new \BadFunctionCallException("Implement the function");
     }
 
-    public function calculateLeftOverSlices()
+    public function calculateLeftOverSlices(int $pizzas, int $number_of_friends)
     {
+        return fmod($pizzas * $this->NUMBER_OF_SLICES, $number_of_friends);
         throw new \BadFunctionCallException("Implement the function");
     }
 }
 
+$pizza_pi = new PizzaPi();
+$grams = $pizza_pi -> calculateDoughRequirement(4, 8);
+$cans_to_buy = $pizza_pi -> calculateSauceRequirement(8, 250);
+$cheese_coverage = $pizza_pi -> calculateCheeseCubeCoverage(25, 0.5, 30);
+$total_leftover = $pizza_pi -> calculateLeftOverSlices(2, 4);
+print($grams . "<br>");
+print($cans_to_buy . "<br>");
+print($cheese_coverage . "<br>");
+print($total_leftover . "<br>");
